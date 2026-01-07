c9f114ff308c5fb6b25fee3ad73d2b17f6412037
vcd44680 <vcd44680@uga.edu>
2026-01-06 20:28:49 -0500

    cleaned things up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,8 @@
-import resumeData from "./resume.json";
-import type { Resume } from "../../types/resume";
-import Nav from "./components/Nav";
-import Header from "./components/Header";
-import Section from "./components/Section";
-import EducationList from "./components/Education";
-import Honors from "./components/Honors";
-import Skills from "./components/Skills";
-import Footer from "./components/Footer";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-
-const resume = resumeData as Resume;
 
 export default function Home() {
-  const { name, tagline, headshot, contact, education, honors_and_awards, skills, leadership_and_experience, projects } = resume;
 
   return (
     <main>
-      <Nav name={name} context='home'/>
-      <Header name={name} headshot={headshot} tagline={tagline} contact={contact}/>
-
-      <Section title="Education">
-        <EducationList {...education}/>
-      </Section>
-
-      <Section title="Honors and Awards">
-        <Honors items={honors_and_awards}/>
-      </Section>
-
-      <Section title="Skills">
-        <Skills skills={skills}/>
-      </Section>
-
-      <Section title="Leadership and Experience">
-        <Experience items={leadership_and_experience}/>
-      </Section>
-
-      <Section title="Projects">
-        <Projects projects={projects}/>
-      </Section>
-      <Footer name={name} link={contact.linkedin}/>
     </main>
   );
 }
